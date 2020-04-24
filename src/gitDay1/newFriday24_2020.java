d2e7116710f7c0d634c5b5bacd7ecef8dd326d3e
joshua.c.millage <millagej@icloud.com>
2020-04-24 15:53:23 -0400

    second commit

---
+++ src/gitDay1/newFriday24_2020.java
@@ -0,0 +1,16 @@
+package gitDay1;
+
+public class newFriday24_2020 {
+    public static void main(String[] args) {
+        String companyName = "Cybertek";
+        String yourName = "Joshua";
+        int employeeID = 257795478;
+        String jobTitle = "District Manager";
+        String salery = "90$ and hour";
+        System.out.println(companyName);
+        System.out.println(yourName);
+        System.out.println(employeeID);
+        System.out.println(jobTitle);
+        System.out.println(salery);
+    }
+}
