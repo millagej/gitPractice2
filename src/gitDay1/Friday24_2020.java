7d279bc1b6f38401e54ea3651b223c646b51ea98
joshua.c.millage <millagej@icloud.com>
2020-04-24 15:03:55 -0400

    first commit

---
+++ src/gitDay1/Friday24_2020.java
@@ -0,0 +1,11 @@
+package gitDay1;
+
+public class Friday24_2020 {
+    public static void main(String[] args) {
+        int city1 = 2345673;
+        int city2 = 5688944;
+        int city3 = 16189445;
+        System.out.println(city1+" "+city2+" "+city3);
+        System.out.println(city1+"\n"+city2+"\n"+city3);
+    }
+}
